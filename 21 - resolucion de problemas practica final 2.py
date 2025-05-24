3027f4afeeabc82e8998d7c6b222ad7917d8552e
Vir <virginiapiazza19@gmail.com>
2025-05-24 20:20:32 -0300

    Se cargan archivos de clases de resolucion de problemas practica final 2

---
+++ 21 - resolucion de problemas practica final 2.py
@@ -0,0 +1,8 @@
+# Paso 1: Solicitar al usuario la medida a convertir
+medida_cms = float(input("Ingrese la medida que desea convertir:"))
+
+# Paso 2: Realizar la conversión
+medida_pulgada = medida_cms / 2.54
+
+# Paso 2: Mstrarle el resultado al usuario
+print("La medida en pulgadas es:", medida_pulgada)

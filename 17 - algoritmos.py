f47ece48df154c3d85d272f1cbc75d930f809126
Vir <virginiapiazza19@gmail.com>
2025-05-24 16:40:19 -0300

    Se cargan archivos de clase de algoritmos

---
+++ 17 - algoritmos.py
@@ -0,0 +1,30 @@
+# ALGORITMOS
+
+# Serie de pasos secuenciales para poder resolver una problemática de forma eficiente
+
+# -------------------------------
+# Paso 1: Solicitar entrada al usuario.
+
+palabra_ingresada = input("Por favor ingrese una palabra: ")
+# -------------------------------
+
+# -------------------------------
+# Paso 2: Contar la cantidad de letras.
+
+cantidad_letras = len(palabra_ingresada)
+# -------------------------------
+
+# -------------------------------
+# Paso 3: Mostrar al usuario el resultado.
+
+print("La palabra ingresada tiene", cantidad_letras, "letras.")
+# -------------------------------
+
+# -------------------------------
+# Código optimizado
+
+contador_letras = len(input("Por favor ingrese una palabra: "))
+
+print("La palabra ingresada tiene", contador_letras, "letras.")
+
+# -------------------------------

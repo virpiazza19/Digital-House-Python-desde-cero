2dc083226442a087cd757287d8cc00e1136572ae
Vir <virginiapiazza19@gmail.com>
2025-05-25 21:09:30 -0300

    Se agregan apuntes de clase de sets

--- segunda parte del curso/05 - sets.py
+++ segunda parte del curso/05 - sets.py
@@ -70,38 +70,92 @@ Al no tener un orden determinado, no se pueden indexar los elementos adentro del
 
 # AGREGAR ITEMS
 
-conjunto_frutas = {
-    "Frambuesa",
-    "Limón",
-    "Melón",
-    "Lima",
-}
-conjunto_frutas.add("Arándano")
+# conjunto_frutas = {
+#     "Frambuesa",
+#     "Limón",
+#     "Melón",
+#     "Lima",
+# }
+# conjunto_frutas.add("Arándano")
+#
+# conjunto_tropicales = {
+#     "Mango",
+#     "Piña",
+#     "Papaya",
+#     "Arándano",  # el duplicado no lo agrega
+# }
+#
+# conjunto_frutas.update(conjunto_tropicales)  # se puede agregar un set a otro set
+#
+# print(conjunto_frutas)
+#
+# lista_frutas = ["Kiwi", "Naranja"]
+#
+# conjunto_frutas.update(lista_frutas) # se puede agregar una lista a un set
+#
+# print(conjunto_frutas)
+#
+# conjunto_frutas.remove("Naranja") # se puede eliminar un elemento del set
+# conjunto_frutas.discard("Papaya") # se puede descartar un elemento del set. Funciona como el remove
+#
+# conjunto_frutas.pop() # al no estar indexado el set, el elemento que se elimina con pop es aleatorio
+#
+# conjunto_frutas.clear() # devuelve None
+#
+# # del conjunto_frutas # al tratar de imprimir el set va a tirar error de código, ya que deja de existir el mismo
+#
+# print(conjunto_frutas)
 
-conjunto_tropicales = {
-    "Mango",
-    "Piña",
-    "Papaya",
-    "Arándano",  # el duplicado no lo agrega
-}
+# -------------------------------
+"""-------------------"""
+# -------------------------------
 
-conjunto_frutas.update(conjunto_tropicales)  # se puede agregar un set a otro set
+# JUNTAR EN SETS
 
-print(conjunto_frutas)
+# conjunto_letras = {"a", "b", "c"}
+# conjunto_numeros = {1, 2, 3}
+#
+# # union = conjunto_letras.union(conjunto_numeros)
+# union = conjunto_letras | conjunto_numeros
+#
+# print(union)
 
-lista_frutas = ["Kiwi", "Naranja"]
+# conjunto_letras = {"a", "b", "c"}
+# tupla_numeros = (1, 2, 3)
+#
+# # union = conjunto_letras | tupla_numeros # en este caso no se puede unir un set y una tupla
+# union = conjunto_letras.union(tupla_numeros) # devuelve un set que se lo asigna a una variable de tipo set
+#
+# conjunto_letras.update(tupla_numeros) # en este caso no devuelve set, ya que está actualizando el mismo conjunto
+#
+# print(union)
+# print(conjunto_letras)
 
-conjunto_frutas.update(lista_frutas) # se puede agregar una lista a un set
+## set_chicago = {"Python", "JavaScript", "AWS"}
+## set_oxford = {"Python", "TypeScript", "GoogleCloud"}
+##
+## set3 = set_chicago.intersection(set_oxford)  # valores en comun
+## set3 = set_chicago & set_oxford  # valores en comun
+##
+## print(set3)
+##
+## # set_chicago.intersection_update(set_oxford)  # actualiza el conjunto dejando solo aquellos valores que se repiten en ambos conjuntos
+## # print(set_chicago)
+##
+## set4 = set_chicago.difference(set_oxford) # valores diferentes de set_chicago
+## print(set4)
+##
+## set5 = set_oxford.difference(set_chicago) # valores diferentes de set_oxford
+## print(set5)
+##
+## set6 = set_chicago - set_oxford # elimina del primer set los elementos que se encuentran en el segundo
+## print(set6)
 
-print(conjunto_frutas)
+# BUCLES EN SETS
 
-conjunto_frutas.remove("Naranja") # se puede eliminar un elemento del set
-conjunto_frutas.discard("Papaya") # se puede descartar un elemento del set. Funciona como el remove
+conjunto_frutas = {"Frambuesa","Limón","Melón","Lima"}
 
-conjunto_frutas.pop() # al no estar indexado el set, el elemento que se elimina con pop es aleatorio
+for fruta in conjunto_frutas:
+    print(fruta)
 
-conjunto_frutas.clear() # devuelve None
-
-# del conjunto_frutas # al tratar de imprimir el set va a tirar error de código, ya que deja de existir el mismo
-
-print(conjunto_frutas)
+""" Al no tener índices, no se pueden utilizar WHILE, RANGE """

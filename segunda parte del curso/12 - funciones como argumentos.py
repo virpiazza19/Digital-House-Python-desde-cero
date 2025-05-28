174049971bd2180340e0861a4679a5b3d551af0a
Vir <virginiapiazza19@gmail.com>
2025-05-28 20:00:01 -0300

    Se agregan apuntes de funciones como argumentos

--- segunda parte del curso/12 - funciones como argumentos.py
+++ segunda parte del curso/12 - funciones como argumentos.py
@@ -1,2 +1,17 @@
 # FUNCIONES COMO ARGUMENTOS
 
+# Callbacks -> función por argumento de otra función para que resuelve algo puntual
+
+def aplicar_funcion(func,valor):
+    return func(valor)
+
+def cuadrado(x):
+    return x*x
+
+def cubo(x):
+    return x*x*x
+
+print(aplicar_funcion(cuadrado,3)) # Devuelve 9
+
+print(aplicar_funcion(cubo,3)) # Devuelve 27
+
